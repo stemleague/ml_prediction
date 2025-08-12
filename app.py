0119fcc70ed5b9fc2b62d2abacca7d2a099975a4
stem league <64894084+stemleague@users.noreply.github.com>
2025-08-11 19:11:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def serve_index():
     return send_from_directory("static", "index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
